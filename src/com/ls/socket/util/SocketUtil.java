8d98ef6c84891025abba12e118b34a77e1784058
yingqingFan <732722340@qq.com>
2019-04-03 17:58:24 +0800

    add readme

--- src/com/ls/socket/util/SocketUtil.java
+++ src/com/ls/socket/util/SocketUtil.java
@@ -1,7 +1,5 @@
 package com.ls.socket.util;
 
-import com.ls.socket.server.SocketServer;
-
 public class SocketUtil {
     public static final String LINE_SEPARATOR = System.getProperty("line.separator");
     public static final String[] ACTIONS = new String[]{"send message", "view history", "view online clients", "bind", "heartbeat", "init send", "power down"};
